3ac37d9831e1adca8c2a3295190b4eac71dc3072
Anish Kumar Yadav <61132432+yadavAnish@users.noreply.github.com>
2026-01-03 08:52:08 +0545

    Add logging to HL7Service class

--- pacs-integration-webapp/src/main/java/org/bahmni/module/pacsintegration/services/HL7Service.java
+++ pacs-integration-webapp/src/main/java/org/bahmni/module/pacsintegration/services/HL7Service.java
@@ -22,9 +22,13 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class HL7Service {
+    private static final Logger logger = LoggerFactory.getLogger(PatientIdentificationMapperImpl.class);
+
 
     @Autowired
     private OrderRepository orderRepository;
